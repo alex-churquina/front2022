1cec42380cd2f2abc75d465fc1443a0fb3fa4011
Alex <alexchurquina@tdf.edu.ar>
2022-08-02 20:09:28 -0300

    Nuevos

--- LEANDRO/trabajo1.js
+++ LEANDRO/trabajo1.js
@@ -1,6 +1,25 @@
-let miLista = document.querySelector("ol");
+let botonNuevo = document.getElementById("Boton");
+botonNuevo.addEventListener("click",chip);
 
-let agregadoNuevo = document.createElement("li");
-agregadoNuevo.innerHTML = ingrese.value;
+ function chip(){
+    let miLista = document.querySelector("ol");
+    let ingrese = document.getElementById("ingrese");
+    let agregadoNuevo = document.createElement("li");
+    agregadoNuevo.innerHTML = ingrese.value;
+    miLista.appendChild(agregadoNuevo); 
+ }
 
-miLista.appendChild(agregadoNuevo); 
+
+
+ let miButon = document.getElementById("miButon");
+ miButon.addEventListener("click",agregarTexto);
+
+function agregarTexto(){
+    let esTexto = document.getElementById("divParrafo");
+    let inputParrafo = document.getElementById("inputParrafo");
+    let textoAgregado = document.createElement("p");
+    textoAgregado.innerHTML = inputParrafo.value;
+    esTexto.appendChild(textoAgregado);
+
+}
+
