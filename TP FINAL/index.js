e3f8aa4f088d2458fb37cdb3a1770b48c97dfa70
Alex <alexchurquina@tdf.edu.ar>
2022-12-16 22:53:44 -0300

    Tp Final

--- TP FINAL/index.js
+++ TP FINAL/index.js
@@ -1,17 +1,17 @@
 async function obtenerDatos(){
-    let respuesta = await fetch("https://6398b453fe03352a94dbe15d.mockapi.io/api/empleados")
-    let datos = await respuesta.json();
+    const respuesta = await fetch("https://6398b453fe03352a94dbe15d.mockapi.io/api/empleados")
+    const datos = await respuesta.json();
     console.log(datos);
 
     datos.forEach(element => {
 
         document.getElementById('table').innerHTML+=`  <tr>
-             <td> ${element.nombre}  ${element.apellido}</td>
-             <td>${element.area}</td>
+
+             <td>${element.nombre} ${element.apellido}</td>
+            
              <td><img src="${element.foto}"></td>
              <td><button class="ver" id="${element.id}">Ver</button></td>
-             </tr>`      
-    
+             </tr>`            
    });
 
    let btn = document.querySelectorAll(".ver");
@@ -24,56 +24,48 @@ async function obtenerDatos(){
 } 
 
 async function mostrarUno(id){
-    let respuesta = await fetch("https://6398b453fe03352a94dbe15d.mockapi.io/api/empleados/"+id,)
-    let datos = await respuesta.json();
+    const respuesta = await fetch("https://6398b453fe03352a94dbe15d.mockapi.io/api/empleados/"+id)
+    const datos = await respuesta.json();
     console.log(datos);
 
-    
-    let empleado = document.getElementById("personal"); //para mostrar mi lista con los datos , no olvidarme preguntar.
+    let empleado = document.getElementById("empleado");
     empleado.innerHTML = "";
 
-    let nombreApellido = document.createElement("h4");
-    nombreApellido.innerHTML = datos.nombre +"   "+ datos.apellido;
-    
+    let nombre = document.createElement("h4");
+    nombre.innerHTML = datos.nombre;
 
-    let area = document.createElement("h4");
-    area.innerHTML = datos.area;
+    let apellido = document.createElement("h4");
+    apellido.innerHTML = datos.apellido;
 
     let imagen = document.createElement("h4");
     imagen.innerHTML = `<img src="${datos.foto}"></img>`
 
     
-    personal.appendChild(nombreApellido);
-    personal.appendChild(area);
-    personal.appendChild(imagen);
+    empleado.appendChild(nombre);
+    empleado.appendChild(apellido);
+    empleado.appendChild(imagen);
 
 }
 
-    let datos ={     
+let misDatos ={
+    "nombre":"Dario",
+    "apellido":"Lopez",
+    "area":"Developed",
+    "domicilio":"Rio Grande",
+    "foto":"https://http2.mlstatic.com/D_NQ_NP_809466-MLA46695132291_072021-O.jpg",
+    "id":"3"
+}
 
-    "nombre":"Devo",
-    "apellido":"Maidana",
-    "foto": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/baby-yoda-nombre-1606753349.jpg?crop=1.00xw:1.00xh;0,0&resize=980:* ",
-    "id":"7"
-    
-    
-    
-    }
-    
-    
-    async function modificarDatos(datos){
-        console.log("Dato Modificar");
-       const respuesta = await fetch ("https://6398b453fe03352a94dbe15d.mockapi.io/api/nombre/",{
-                         method:"PUT",
-                         body:JSON.stringify(datos),
-                         headers:{ "content-type":"application/json"}
-                         
-    
+async function modificarDatos(misDatos){
+    const respuesta = await fetch("https://6398b453fe03352a94dbe15d.mockapi.io/api/empleados/"+misDatos.id, {
+        method: "PUT",
+        body:JSON.stringify(misDatos),
+        headers:{"Content-type":"application/json"}
     });
-                        const data = await respuesta.json();
-                            console.log(data);
-    
-    
+    const data = await respuesta.json();
+        console.log(data);
 }
 
+
+modificarDatos(misDatos);
 obtenerDatos();
